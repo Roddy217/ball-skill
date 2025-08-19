4dfeb39b2378ca2b2d8459d6382f21e689998648
Roddy Wiley <roddy806@hotmail.com>
2025-08-18 22:22:40 -0400

    Ensured proper Firebase auth initialization

---
+++ services/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIRR9apJ4btQMvGytLkplGOTbFJOySkPU",
+  authDomain: "ballskill-app.firebaseapp.com",
+  projectId: "ballskill-app",
+  storageBucket: "ballskill-app.firebasestorage.app",
+  messagingSenderId: "1007534039189",
+  appId: "1:1007534039189:web:e7748176b4aad974d1bb9c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
